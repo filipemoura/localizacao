13c1e2ef01e454b22fe02011e0731daf2b8906c1
Filipe Alves <filipemouraalves@gmail.com>
2022-03-15 08:51:21 -0300

    Adicionando documentação da API

--- src/main/java/br/com/mob7/localizacao/api/LocalizacaoApplication.java
+++ src/main/java/br/com/mob7/localizacao/api/LocalizacaoApplication.java
@@ -3,7 +3,10 @@ package br.com.mob7.localizacao.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class LocalizacaoApplication {
 
 	public static void main(String[] args) {
